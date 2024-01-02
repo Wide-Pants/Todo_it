18f56fc535b7306d3e28cd95936f059647f0d626
Wide-Pants <35173697+Wide-Pants@users.noreply.github.com>
2024-01-02 17:16:51 +0900

    20240102raon

--- main.js
+++ main.js
@@ -55,7 +55,7 @@ dolists.forEach(element => {
 cnt = 0
 addbutton.addEventListener(`click`, ()=>{
     const new_todo_input= document.createElement(`li`);
-    new_todo_input.innerHTML = `<div style="margin-left:10px;" class="ch_button"></div><input type="text" id="writen" style="font-size:16px; margin-left:5px; outline: none; border:none;">`
+    new_todo_input.innerHTML = `<div style="margin-left:10px;" class="ch_button"></div><div id="list_txt"><input type="text" id="writen" style="font-size:16px; line-height: 16px; heigth:24px; outline: none; border:none;"></div>`
     todo_list.append(new_todo_input)
     cnt++;
     
@@ -65,9 +65,22 @@ addbutton.addEventListener(`click`, ()=>{
     todo_text.addEventListener(`focusout`, ()=>{
         thing = todo_text.value
         new_todo= document.createElement(`li`);
-        new_todo.innerHTML = `<div class="can_mov_bt"></div><input type="checkbox" class="inv" id="s${cnt}"><label for="s${cnt}" class="ch_button"></label><span id="list_txt">${thing}</span>`
+        new_todo.innerHTML = `<div class="can_mov_bt inv"></div><input type="checkbox" style="display:none;" id="s${cnt}"><label for="s${cnt}" class="ch_button"></label><div id="list_txt"><span>${thing}</span></div>`
         new_todo_input.remove();
         todo_list.append(new_todo)
+        let dd=1
+        const list_txt = document.getElementById(`list_txt`)
+        list_txt.addEventListener(`mouseover`,(e)=>{
+            can_mov = list_txt.parentElement.children[0]
+            can_mov.classList.toggle(`inv`)
+            list_txt.children[0].style.background = `#EEEEEE`
+            list_txt.children[0].style.borderRadius = `2.5px`
+        })
+        list_txt.addEventListener(`mouseout`,()=>{
+            can_mov = list_txt.parentElement.children[0]
+            can_mov.classList.toggle(`inv`)
+            list_txt.children[0].style.background = `none`
+        })
     })
     todo_text.addEventListener("keyup", function (event) {
         if (event.code == 'Enter'|| event.code == 'NumpadEnter') {
@@ -75,4 +88,3 @@ addbutton.addEventListener(`click`, ()=>{
             todo_text.blur();       
     }})
 })
-
