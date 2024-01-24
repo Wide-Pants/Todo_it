f2736a1a84cc977a0e8bd7f0f770cbd73143770c
Wide-Pants <35173697+Wide-Pants@users.noreply.github.com>
2024-01-24 16:16:30 +0900

    Update route.js raon

--- js/route.js
+++ js/route.js
@@ -1,16 +1,24 @@
 const route = ()=>{
     const url = new URL(window.location.href);
     console.log(url.pathname)
-    if(url.pathname==`/cat`){
-        switch(url.search){
-            case '?=3':
-                to_do_list_box.innerHTML = ``;
-                main_contents_zone.style.display = `grid`;
-                main_homepage.style.display = `none`;
-                load_list(3)
+    const page_length = 5
+    if(url.pathname==`/cat`&& url.search.substring(0, 2)==`?=`){
+
+        const page_num = parseInt(url.search.substring(2), 10);
+
+        if (!isNaN(page_num)) {
+            if(page_num<page_length){
+                to_do_list_box.innerHTML = '';
+                main_contents_zone.style.display = 'grid';
+                main_homepage.style.display = 'none';
+                load_list(page_num);
+                return;
             }
+        }
+        console.error('Invalid page number format'); // 또는 다른 오류 처리 방법 사용
+        return; // 오류가 발생하면 함수 종료
     }
 }
-
 window.addEventListener('hashchange', route);
 window.addEventListener('DOMContentLoaded', route);
+window.addEventListener(`popstate`,route)
